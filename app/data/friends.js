6b774b34cebb03f943e9511e2615924e37ae0968
Nigel John <mytrini@att.net>
2016-06-28 02:19:09 -0400

    Updated all files

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+	{
+		'name': 'John',
+		'photo': "",
+		'scores': [5, 3, 4, 2, 5, 2, 4, 3, 5, 4]
+	},
+
+	{
+		'name': 'Erick',
+		'photo': "",
+		'scores': [5, 3, 4, 2, 5, 3, 4, 2, 5, 3]
+	},
+
+	{
+		'name': 'Christine',
+		'photo': "",
+		'scores': [5, 4, 3, 5, 2, 4, 3, 5, 3, 4]
+	},
+
+	{
+		'name': 'Susan',
+		'photo': "",
+		'scores': [3, 5, 4, 2, 5, 3, 4, 5, 3, 2]
+	},
+
+	{
+		'name': 'Paul',
+		'photo': "",
+		'scores': [5, 3, 4, 2, 4, 3, 5, 2, 4, 5]
+	},
+
+	{
+		'name': 'Lynda',
+		'photo': "",
+		'scores': [3, 4, 2, 5, 3, 4, 3, 5, 3, 3]
+	}
+];
+
+module.exports = friends;
